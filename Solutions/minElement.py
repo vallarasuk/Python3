5219ba95e1bc753f4fd92038a29ef8eab6dfceb8
vallarasuk <vallarasu.kanthasamy@thidiff.com>
2024-07-05 18:21:30 +0530

    minimum values solved

---
+++ Solutions/minElement.py
@@ -0,0 +1,11 @@
+# Minimum Element
+# Question: Find the minimum element in a list of integers.
+# Example Input: [10, 20, 30, 40, 5]
+# Example Output: 5
+
+def find_min_value(values):
+    return min(values)
+
+values = [ 10,20,30, 40,5]
+
+print(find_min_value(values))
