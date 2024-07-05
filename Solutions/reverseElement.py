d7a851cc58bfc742879822f556764430f23556e0
vallarasuk <vallarasu.kanthasamy@thidiff.com>
2024-07-05 19:31:52 +0530

    reverse element another solution added

--- Solutions/reverseElement.py
+++ Solutions/reverseElement.py
@@ -10,3 +10,17 @@ def find_reverse(values):
 values = [1,2,3,4,5]
 
 print(find_reverse(values))
+
+
+# input list
+lst = [10, 11, 12, 13, 14, 15]
+# the above input can also be given as
+# lst=list(map(int,input().split()))
+l = []  # empty list
+
+# iterate to reverse the list
+for i in lst:
+    # reversing the list
+    l.insert(0, i)
+# printing result
+print(l)
