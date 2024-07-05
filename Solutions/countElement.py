156a58c912ff445f1e05e6a0e37ce5d074c2ed35
vallarasuk <vallarasu.kanthasamy@thidiff.com>
2024-07-05 18:28:21 +0530

    count element solved

---
+++ Solutions/countElement.py
@@ -0,0 +1,11 @@
+# Count of Elements
+# Question: Count the number of elements in a list.
+# Example Input: [5, 10, 15, 20]
+# Example Output: 4
+
+
+def find_count(values):
+    return len(values)
+
+values = [ 5,10,15,20]
+print(find_count(values))
